2e58244a3b79e29c06a8f19aff97253fc1bb62bb
bminczu <bminczu@gmail.com>
2020-10-29 21:01:10 -0500

    commit tree b4f6bce5d4383700568aec7bc75204f411f696e1

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
-class BankAccount
 
-end
+class BankAccount
+    attr_accessor  :balance, :status
+    attr_reader :name
+        def initialize(name, balance = 1000, status = "open")
+            @name = name
+            @balance = balance
+            @status = status
+        end
+        def deposit(balance)
+            @balance += 1000
+        end
+        def display_balance
+            "Your balance is $#{self.balance}."
+        end
+
+        def valid?
+        balance > 0 && status =="open"
+        end
+
+        def close_account
+            self.status = "closed"
+        end
+
+    end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_accessor :receiver, :sender, :status, :amount
+
+  def initialize(sender, receiver, amount, status ='pending')
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid? && self.sender.balance >= amount
+    
+  end
+  def execute_transaction
+    if status != "complete" && valid?
+    sender.balance -= amount
+    receiver.balance += amount
+    @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
 end
+
+
+
+
+
+
+
